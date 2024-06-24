02a90062cebdac56d8948c72f681b43418c33172
Dmytro Chapovskyi <dchapovskyi@shutterstock.com>
2024-06-24 10:23:37 +0200

    initial commit

---
+++ GitHubSample/GitHubSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubSampleApp.swift
+//  GitHubSample
+//
+//  Created by Dmytro Chapovskyi on 24.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct GitHubSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
